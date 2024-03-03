cdd91dbaefa329da2f47ef07448f6e3f6c5183b8
Nascimento <jacksoncpn@outlook.com.br>
2024-03-03 09:51:52 -0300

    Menu e rotas de Navegação ADD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
+import { HomeComponent } from './Page/home/home.component';
+import { SobreComponent } from './Page/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'Home', component: HomeComponent},
+{path: 'Sobre', component: SobreComponent},
+
+];
